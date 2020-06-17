c67626acbbdfbf95137022fad29fc96bdeddfa7c
mani <manijamali2003@gmail.com>
2020-06-17 21:29:42 +0430

    Commit Server files

--- configure.py
+++ configure.py
@@ -4,3 +4,12 @@ import os
 os.mkdir ('desk')
 os.mkdir ('disk')
 os.mkdir ('pack')
+os.mkdir('etc')
+os.mkdir('etc/hosts')
+file = open ('etc/distro','w')
+file.write("""
+name: Pyabr Server
+version: 0.0.2
+cs: https://github.com/manijamali2003/pyabr
+""")
+file.close()
